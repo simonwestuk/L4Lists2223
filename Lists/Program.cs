b8770c0dd45868bee42061ff3c0d5f9d8de6cd97
wests <wests@C-010053>
2022-11-01 14:40:52 +0000

    Init

--- Lists/Program.cs
+++ Lists/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Collections.Generic;
+
+List<string> dinos = new List<string>();
+
+dinos.Add("Simon");
+dinos.Add("Sam");
+dinos.Add("Sally");
+dinos.Add("Sue");
+dinos.Add("Sill");
+
+foreach (string dino in dinos)
+{
+    //Console.WriteLine(dino);
+}
+
+Console.WriteLine(dinos[2]);
